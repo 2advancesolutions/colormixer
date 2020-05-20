54252d63c958f4894f81084fb96b39056e891eac
Bellas, Reginald <Reginald.Bellas@usfoods.com>
2020-05-20 14:35:31 -0500

    added unsubscribe

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ColorService } from './services/color.service';
+import { Subscription } from 'rxjs';
 
 interface IColor {
   color1: string;
   color2: string;
 }
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   color1: string;
   color2: string;
   loaded: boolean;
- constructor(private colorService: ColorService) {}
+  sub: Subscription;
+  constructor(private colorService: ColorService) { }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
   ngOnInit(): void {
-   this.colorService.getColors().subscribe((data: IColor) => {
-     this.color1 = '#' + data.color1;
-     this.color2 = '#' + data.color2;
-     this.loaded = true;
-   });
+    this.sub = this.colorService.getColors()
+      .subscribe((data: IColor) => {
+        this.color1 = '#' + data.color1;
+        this.color2 = '#' + data.color2;
+        this.loaded = true;
+      });
   }
 }
